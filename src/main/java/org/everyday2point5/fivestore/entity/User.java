17bee31443aff7426b57226c288f9d51751f1c32
LWJCODER <15818464582@163.com>
2016-12-21 14:37:00 +0800

    ...

--- src/main/java/org/everyday2point5/fivestore/entity/User.java
+++ src/main/java/org/everyday2point5/fivestore/entity/User.java
@@ -13,29 +13,36 @@ import org.hibernate.annotations.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class User extends BaseEntity implements Serializable{
+public class User extends BaseEntity implements Serializable {
+
 	String account;
 	String password;
-	public String user_name;
-	public String avatar;
+	String user_name;
+	String avatar;
 	String email;
 	float money;
+
 	public String getUser_name() {
 		return user_name;
 	}
+
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
 	}
+
 	public float getMoney() {
 		return money;
 	}
+
 	public void setMoney(float money) {
 		this.money = money;
 	}
-	@Column(unique=true)
+
+	@Column(unique = true)
 	public String getAccount() {
 		return account;
 	}
+
 	@Column(nullable = false)
 	@JsonIgnore
 	public String getPassword() {
@@ -45,25 +52,29 @@ public class User extends BaseEntity implements Serializable{
 	public String getAvatar() {
 		return avatar;
 	}
+
 	@Column(nullable = false)
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setAccount(String account2) {
 		this.account = account2;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public void setAvatar(String avatar) {
 		this.avatar = avatar;
 	}
 
-	public String getCurrentUser(HttpServletRequest request){
+	public String getCurrentUser(HttpServletRequest request) {
 		return getAccount();
 
 	}
