121ef983fe6bdb62f8dfe2c25c8dba239eba54c6
LWJCODER <15818464582@163.com>
2016-12-22 19:47:14 +0800

    ..

---
+++ src/main/java/org/everyday2point5/fivestore/entity/MyOrder.java
@@ -0,0 +1,120 @@
+package org.everyday2point5.fivestore.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import org.everyday2point5.fivestore.util.BaseEntity;
+
+@Entity
+public class MyOrder  extends BaseEntity implements Serializable{
+	int
+	status,
+	amount,
+	order_num,
+	goods_id;
+	
+	String name, phone, address;
+	Date createDate, editDate;
+	
+	@Column
+	public int getGoods_id() {
+		return goods_id;
+	}
+
+	public void setGoods_id(int goods_id) {
+		this.goods_id = goods_id;
+	}
+
+
+	
+	@Column(updatable = false)
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	@Column
+	public Date getEditDate() {
+		return editDate;
+	}
+
+	public void setEditDate(Date editDate) {
+		this.editDate = editDate;
+	}
+
+	@PreUpdate
+	void onPreUpdate(){
+		editDate = new Date();
+	}
+	
+
+
+	@PrePersist
+	void onPrePresist(){
+		createDate = new Date();
+		editDate = new Date();
+	}
+	
+	@Column
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Column(nullable = true)
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	@Column(nullable = true)
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Column
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	@Column
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	@Column(unique = true)
+	public int getOrder_num() {
+		return order_num;
+	}
+
+	public void setOrder_num(int order_num) {
+		this.order_num = order_num;
+	}
+
+}
---
+++ src/main/java/org/everyday2point5/fivestore/service/IOrderService.java
@@ -0,0 +1,9 @@
+package org.everyday2point5.fivestore.service;
+
+import org.everyday2point5.fivestore.entity.MyOrder;
+
+public interface IOrderService {
+
+	MyOrder save(MyOrder order);
+
+}
