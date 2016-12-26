57d2845fafa15f38210f06efa9e2fccf848a315e
Administrator <Administrator@7A304-0039>
2016-12-26 16:01:15 +0800

    完善“私信”功能1

--- src/main/java/org/everyday2point5/fivestore/entity/InboxList.java
+++ src/main/java/org/everyday2point5/fivestore/entity/InboxList.java
@@ -3,42 +3,35 @@ package org.everyday2point5.fivestore.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.PrePersist;
 
 import org.everyday2point5.fivestore.util.BaseEntity;
 import org.springframework.stereotype.Component;
+
 @Component
 @Entity
-public class Inbox extends BaseEntity implements Serializable{
-	
-	/*私信内容
-	 * 发送者id
-	 * 接收者id
-	 * 是否已读
-	 * 时间
-	 */
+public class InboxList extends BaseEntity implements Serializable{
+
 	
 	
-	String inboxContent;
+	Inbox last_inbox;
+	String sign;
 	String send_name;
 	String rec_name;
-	boolean isread;
 	Date createDate;
-	String sign;
 	
+	
+	public Inbox getLast_inbox() {
+		return last_inbox;
+	}
+	public void setLast_inbox(Inbox last_inbox) {
+		this.last_inbox = last_inbox;
+	}
 	public String getSign() {
 		return sign;
 	}
 	public void setSign(String sign) {
 		this.sign = sign;
-	}
-	public String getInboxContent() {
-		return inboxContent;
-	}
-	public void setInboxContent(String content) {
-		this.inboxContent = content;
 	}
 	public String getSend_name() {
 		return send_name;
@@ -52,23 +45,12 @@ public class Inbox extends BaseEntity implements Serializable{
 	public void setRec_name(String rec_name) {
 		this.rec_name = rec_name;
 	}
-	public boolean isIsread() {
-		return isread;
-	}
-	public void setIsread(boolean isread) {
-		this.isread = isread;
-	}
-	
-	@Column(updatable=false)
 	public Date getCreateDate() {
 		return createDate;
 	}
 	public void setCreateDate(Date createDate) {
 		this.createDate = createDate;
 	}
-
-	@PrePersist
-	void onPrePersist(){
-		createDate = new Date();
-	}
+	
+	
 }
--- src/main/java/org/everyday2point5/fivestore/service/DefaultInboxService.java
+++ src/main/java/org/everyday2point5/fivestore/service/DefaultInboxService.java
@@ -1,6 +1,8 @@
 package org.everyday2point5.fivestore.service;
 
 import org.everyday2point5.fivestore.entity.Inbox;
+import org.everyday2point5.fivestore.entity.InboxList;
+import org.everyday2point5.fivestore.repository.IInboxListRepository;
 import org.everyday2point5.fivestore.repository.IInboxRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,6 +20,9 @@ public class DefaultInboxService implements IInboxService{
 	@Autowired
 	IInboxRepository inboxRepo;
 	
+	@Autowired
+	IInboxListRepository inboxListRepo;
+	
 	
 	@Override
 	public Inbox save(Inbox inbox){
@@ -25,17 +30,34 @@ public class DefaultInboxService implements IInboxService{
 	}
 	
 	@Override
-	public Page<Inbox> findAllInbox(String name, int i) {
-		Sort sort = new Sort(Direction.DESC, "createDate");
-		PageRequest request = new PageRequest(i, 10, sort);
-		return inboxRepo.findAllInbox(name, request);
+	public InboxList save(InboxList inboxList){
+		return inboxListRepo.save(inboxList);
 	}
+	
+	@Override
+	public InboxList findBySign(String sign){
+		return inboxListRepo.findInboxListBySign(sign);
+	}
+	
+//	@Override
+//	public Page<Inbox> findAllInbox(String name, int i) {
+//		Sort sort = new Sort(Direction.DESC, "createDate");
+//		PageRequest request = new PageRequest(i, 10, sort);
+//		return inboxRepo.findAllInbox(name, request);
+//	}
 	
 
 	@Override
-	public Page<Inbox> searchInbox(String text, int page) {
+	public Page<Inbox> inboxGetChat(String text, int page) {
 		Sort sort = new Sort(Direction.DESC, "createDate");
 		PageRequest request = new PageRequest(page, 10, sort);
-		return inboxRepo.searchInbox(text, request);
+		return inboxRepo.inboxGetChat(text, request);
+	}
+	
+	@Override
+	public Page<InboxList> findInboxList(String text, int page) {
+		Sort sort = new Sort(Direction.DESC, "createDate");
+		PageRequest request = new PageRequest(page, 10, sort);
+		return inboxListRepo.findInboxList(text, request);
 	}
 }
